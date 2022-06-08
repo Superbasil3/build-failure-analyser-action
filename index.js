b5e61e2f4f8f281355bc57394885143917307a86
tbasilien <tbasilien@expediagroup.com>
2022-06-08 21:26:10 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs')
+
+try {
+  const pathLogFile = core.getInput('path-log-file');
+  const githubToken = core.getInput('github-token');
+  const githubDatabaseRepo = core.getInput('github-database-repo');
+
+  console.log(`Go read file at : ${pathLogFile}!`);
+  var data = fs.readFileSync(pathLogFile)
+  console.log(data)
+
+} catch (error) {
+  core.setFailed(error.message);
+}
