919f9071d0abebc417417a0c3e4958a61d793659
tbasilien <tbasilien@expediagroup.com>
2022-06-09 14:56:06 +0200

    Working Version 0.3.3

--- index.js
+++ index.js
@@ -1,67 +1,120 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const lineReader = require('line-reader');
-import { Octokit } from '@octokit/rest';
-
 
 try {
-  const regex = {
-    getRegexHash() {
-      const regexHash = {}
 
-      const jsonData = require('../database.json');
-      const causesEntries = Object.entries(jsonData.causes)
-      const nbrCauses = causesEntries.length
-      let nbrIndications = 0
-      for(const [id, cause] of causesEntries) {
-        for(const indication of cause.indications){
-          let object = {}
-          object['id'] = id
-          object['regex'] = new RegExp(indication);
-          regexHash[indication] = object
-          nbrIndications++
+    const pathLogFile = core.getInput('path-log-file');
+    const githubToken = core.getInput('github-token');
+    const pathLogDatabaseJson = core.getInput('path-log-database-json');
+    const stepOutcome = core.getInput('step-outcome');
+
+    const regex = {
+
+        getRegexHash() {
+            const regexHash = {}
+            const jsonData = require(pathLogDatabaseJson);
+            const causesEntries = Object.entries(jsonData.causes)
+            const nbrCauses = causesEntries.length
+            let nbrIndications = 0
+            for (const [id, cause] of causesEntries) {
+                for (const indication of cause.indications) {
+                    let object = {}
+                    object['id'] = id
+                    object['regex'] = new RegExp(indication);
+                    regexHash[indication] = object
+                    nbrIndications++
+                }
+            }
+            console.log(`Loaded ${nbrCauses} cause(s), including ${nbrIndications} indication(s)`)
+            return Object.entries(regexHash)
         }
-      }
-      console.log(`Loaded ${nbrCauses} cause(s), including ${nbrIndications} indication(s)`)
-      return Object.entries(regex.getRegexHash())
+    };
+
+    const octokit = github.getOctokit(githubToken)
+
+    const pullRequest = {
+        async update(commentsToAdd, owner, repo, issueNumber) {
+            console.log(`issueNumber ${issueNumber} and ${owner} owner and ${repo} repo and ${githubToken}`)
+            const {data: comments} = await octokit.rest.issues.listComments({
+                issue_number: issueNumber,
+                owner: owner,
+                repo: repo
+            })
+            commentsToAdd.push("FAILURE_REPORT")
+            for (const comment of comments) {
+                console.log(`comment.user.login ${comment.user.login}`)
+            }
+            step_failed = stepOutcome === 'failure'
+            const botComment = comments.find(comment => comment.user.login === 'github-actions[bot]' && comment.body.includes("FAILURE_REPORT"))
+            console.debug(`botComment ${botComment}`)
+            if (typeof botComment !== 'undefined' && botComment && !step_failed) {
+                console.debug(`Delete comment : ${botComment.id}`)
+                core.setOutput("status", "Status not in error, removing comment");
+                pullRequest.deleteComments(owner, repo, botComment.id)
+            } else if (typeof botComment !== 'undefined' && botComment) {
+                console.debug(`Replace comment : ${botComment.id}`)
+                commentsToAdd.push("REPLACE_COMMENT")
+                core.setOutput("status", "Status still in error, updating comment");
+                pullRequest.replaceComments(owner, repo, commentsToAdd, botComment.id)
+            } else if (step_failed) {
+                console.debug(`Create comment`)
+                commentsToAdd.push("CREATE_COMMENT")
+                core.setOutput("status", "Status in error, creating comment");
+                pullRequest.createComments(owner, repo, issueNumber, commentsToAdd)
+            }
+        },
+
+        createComments(owner, repo, issueNumber, comments) {
+            octokit.rest.issues.createComment({
+                issue_number: issueNumber,
+                owner: owner,
+                repo: repo,
+                body: comments.join(" and ")
+            })
+        },
+
+        deleteComments(owner, repo, commentId) {
+            octokit.rest.issues.deleteComment({
+                owner: owner,
+                repo: repo,
+                comment_id: commentId
+            })
+        },
+
+        replaceComments(owner, repo, comments, commentId) {
+            octokit.rest.issues.updateComment({
+                owner: owner,
+                repo: repo,
+                comment_id: commentId,
+                body: comments.join(" and ")
+            })
+        }
     }
-  };
 
-
-  const pullRequest = {
-    update(message){
+    const githubContext = github.context;
+    if (githubContext.payload.pull_request == null) {
+        core.setFailed('No pull request found.');
+        throw new Error('No pull request found.');
     }
-  }
 
-  const githubContext = github.context;
-  if (githubContext.payload.pull_request == null) {
-    core.setFailed('No pull request found.');
-    throw new Error('No pull request found.');
-  }
-  const pull_request_number = githubContext.payload.pull_request.number;
+    console.debug(`Go read file at : ${pathLogFile}!`);
+    let regexHash = regex.getRegexHash()
+    let comments = []
+    lineReader.eachLine(pathLogFile, function (line, last) {
+        for (const [regex, regexElement] of regexHash) {
+            if (regexElement.regex.test(line)) {
+                let comment = `Indication ${regex} of cause ${regexElement.id} is matching the line \n ${line}`
+                comments.push(comment)
+                console.debug(comment)
+            }
+        }
+    });
 
-  const pathLogFile = core.getInput('path-log-file');
-  const githubToken = core.getInput('github-token');
-  const githubDatabaseRepo = core.getInput('github-database-repo');
-
-  const octokit = new Octokit({
-    auth: githubToken
-  });
-
-  console.debug(`Go read file at : ${pathLogFile}!`);
-
-  const regexHash = regex.getRegexHash()
-  lineReader.eachLine(pathLogFile, function(line, last) {
-    for(const [regex, regexElement] of regexHash){
-      if(regexElement.regex.test(line)){
-        console.debug(`Indication ${regex} of cause ${regexElement.id} is matching the line \n ${line}`)
-        // Format Message to append in the error message
-      }
-    }
-  });
+    pullRequest.update(comments, githubContext.repo.owner, githubContext.repo.repo, githubContext.issue.number)
 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 
