446d8b52365398c69dd253bfe11ccf6692fb88a6
tbasilien <tbasilien@expediagroup.com>
2022-06-09 10:06:24 +0200

    Make build

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const lineReader = require('line-reader');
+import { Octokit } from '@octokit/rest';
 
-const regex = {
-  getRegexHash() {
-    const regexHash = {}
-
-    const jsonData = require('../database.json');
-    const causesEntries = Object.entries(jsonData.causes)
-    const nbrCauses = causesEntries.length
-    let nbrIndications = 0
-    for(const [id, cause] of causesEntries) {
-      for(const indication of cause.indications){
-        regexHash[indication] = id
-        nbrIndications++
-      }
-    }
-    console.log(`Loaded ${nbrCauses} cause(s), including ${nbrIndications} indication(s)`)
-  }
-};
 
 try {
+  const regex = {
+    getRegexHash() {
+      const regexHash = {}
+
+      const jsonData = require('../database.json');
+      const causesEntries = Object.entries(jsonData.causes)
+      const nbrCauses = causesEntries.length
+      let nbrIndications = 0
+      for(const [id, cause] of causesEntries) {
+        for(const indication of cause.indications){
+          let object = {}
+          object['id'] = id
+          object['regex'] = new RegExp(indication);
+          regexHash[indication] = object
+          nbrIndications++
+        }
+      }
+      console.log(`Loaded ${nbrCauses} cause(s), including ${nbrIndications} indication(s)`)
+      return Object.entries(regex.getRegexHash())
+    }
+  };
+
+
+  const pullRequest = {
+    update(message){
+    }
+  }
+
+  const githubContext = github.context;
+  if (githubContext.payload.pull_request == null) {
+    core.setFailed('No pull request found.');
+    throw new Error('No pull request found.');
+  }
+  const pull_request_number = githubContext.payload.pull_request.number;
+
   const pathLogFile = core.getInput('path-log-file');
   const githubToken = core.getInput('github-token');
   const githubDatabaseRepo = core.getInput('github-database-repo');
 
-  console.log(`Go read file at : ${pathLogFile}!`);
+  const octokit = new Octokit({
+    auth: githubToken
+  });
+
+  console.debug(`Go read file at : ${pathLogFile}!`);
 
   const regexHash = regex.getRegexHash()
   lineReader.eachLine(pathLogFile, function(line, last) {
-
-    var re = new RegExp("ab+c");
-
-    console.log('Line ' + line);
-    if(last) {
-      console.log('Last line printed.');
+    for(const [regex, regexElement] of regexHash){
+      if(regexElement.regex.test(line)){
+        console.debug(`Indication ${regex} of cause ${regexElement.id} is matching the line \n ${line}`)
+        // Format Message to append in the error message
+      }
     }
   });
 
